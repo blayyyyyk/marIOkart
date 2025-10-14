4d9df08e92d61399f521c4ffa76298e81ad232b8
Blake Moody <32070003+gg-blake@users.noreply.github.com>
2025-10-14 19:24:32 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-
+import sys, os
+sys.path.insert(0, os.path.abspath("../src"))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
