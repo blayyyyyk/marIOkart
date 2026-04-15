cdc56901825219a1043e91e0c00bdc0d291239d4
Blake Moody <32070003+blayyyyyk@users.noreply.github.com>
2026-04-15 19:39:05 -0400

    added functionality for reading cli args directly from a json file with `python -m mariokart_ml -c config.json [command]`

--- src/mariokart_ml/__main__.py
+++ src/mariokart_ml/__main__.py
@@ -1,3 +1,5 @@
+import json
+import sys
 from argparse import ArgumentParser
 from .scripts.record import record_parser
 from .scripts.debug import debug_parser
@@ -7,11 +9,22 @@ from .scripts.process import process_parser
 from .scripts.train_rl import train_rl_parser
 from .scripts.util import general_parser, window_parser
 
+# intercept the existing parser to accept json files
+config_parser = ArgumentParser(add_help=False)
+config_parser.add_argument("-c", "--config", type=str, help="Path to a JSON config file")
+pre_args, remaining_argv = config_parser.parse_known_args()
+json_defaults = {}
+if pre_args.config:
+    with open(pre_args.config, "r") as f:
+        json_defaults = json.load(f)
 
 parser = ArgumentParser(
     prog="main.py",
     description="Main entry point for the Mario Kart DS ML visualization application.",
 )
+
+parser.add_argument("-c", "--config", type=str, help="Path to a JSON config file")
+
 subparsers = parser.add_subparsers(dest="command")
 subparsers.add_parser("record", parents=[record_parser, general_parser, window_parser])
 subparsers.add_parser("train", parents=[train_parser, general_parser])
@@ -20,12 +33,18 @@ subparsers.add_parser("eval", parents=[eval_parser, general_parser, window_parse
 subparsers.add_parser("process", parents=[process_parser, general_parser])
 subparsers.add_parser("debug", parents=[debug_parser, general_parser, window_parser])
 
-# parse arguments
-args = parser.parse_args()
+# inject json file contents into parser defaults
+parser.set_defaults(**json_defaults)
+args = parser.parse_args(remaining_argv)
+
 if hasattr(args, "func"):
     args_list = vars(args)
     func = args_list.pop("func")
     args_list.pop("command")
+    
+    # remove leftover config arg from main parser
+    args_list.pop("config", None) 
+    
     func(**args_list)
 else:
     parser.print_help() # print help if no/invalid mode specified
