95867886231c58cb274c90bb7ed2d623e37937c4
Blake Moody <32070003+blayyyyyk@users.noreply.github.com>
2026-04-20 20:30:39 -0400

    added support for reusing prexisting save slots for save state sampling

--- src/mariokart_ml/scripts/train_rl.py
+++ src/mariokart_ml/scripts/train_rl.py
@@ -1,19 +1,29 @@
-from argparse import ArgumentParser
+from argparse import SUPPRESS, ArgumentParser
 from pathlib import Path
 from typing import Optional, Union, cast
+
 import numpy as np
+import torch
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
 from stable_baselines3.common.type_aliases import GymEnv
-from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
-from torch import device
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-from mariokart_ml.wrappers.window_wrapper import VecWindowWrapper, WindowWrapper, VecWindowWrapperSB3
-
-from ..config import ALGO_KWARGS, ALGO_MAP, EPOCHS, PROCESSED_GOOD_DATASET_PATH
-from ..environments import EnvManager
-from ..utils import collect_dsm
-from ..utils.sav_to_dsm import COURSE_ABBREVIATIONS
+from mariokart_ml.config import (
+    ALGO_KWARGS,
+    ALGO_MAP,
+    DEFAULT_DEVICE_NAME,
+    PROCESSED_GOOD_DATASET_PATH,
+    TOTAL_TRAINING_TIMESTEPS,
+)
+from mariokart_ml.environments import EnvManager
+from mariokart_ml.utils import collect_dsm
+from mariokart_ml.utils.sav_to_dsm import COURSE_ABBREVIATIONS
+from mariokart_ml.wrappers.window_wrapper import (
+    VecWindowWrapper,
+    VecWindowWrapperSB3,
+    WindowWrapper,
+)
 
 
 class WindowUpdateCallback(BaseCallback):
@@ -22,8 +32,17 @@ class WindowUpdateCallback(BaseCallback):
         self.window = window
 
     def _on_step(self) -> bool:
-        self.window.update()
-        return self.window.is_alive
+        rewards = self.locals.get("rewards")
+        if rewards is not None:
+            # For multiple processes, you might want to average them or print the first one
+            avg_reward = np.mean(rewards)
+            print(f"Step: {self.num_timesteps} | Mean Reward: {avg_reward:.4f}", end="\r")
+
+        if self.window is not None:
+            self.window.update()
+            return self.window.is_alive
+        else:
+            return True
 
 
 SPARSE_KEYMAP = {
@@ -33,48 +52,61 @@ SPARSE_KEYMAP = {
 }
 
 def train_rl(
-    env_name: str,
-    algorithm: str,
-    epochs: int,
-    scale: int,
-    num_procs: int,
+    algorithm: str = "ppo",
+    total_timesteps: int = TOTAL_TRAINING_TIMESTEPS,
+    scale: int = 1,
+    num_procs: int = 1,
     verbose: bool = False,
     window: bool = False,
-    reuse_save_slots: Optional[bool] = None,
+    reuse_save_slots: bool = False,
+    env_name: str = "mariokart_ml/TimeTrial-v1",
     sample_from: Optional[Path] = None,
+    save_model_path: Optional[Path] = None,
+    load_model_path: Optional[Path] = None,
+    device: torch.device = torch.device(DEFAULT_DEVICE_NAME),
     **kwargs
 ):
     algo_class = ALGO_MAP[algorithm]
     algo_kwargs = ALGO_KWARGS.get(algorithm, {})
 
     movie_paths = [sample_from] * num_procs
-    mgr = EnvManager(env_name, "train", autoreset=True, reuse_save_slots=reuse_save_slots or not window)
+    mgr = EnvManager(
+        env_name,
+        mode="train",
+        autoreset=True,
+        reuse_save_slots=reuse_save_slots or not window
+    )
 
-    callback = None
     if window:
         env = mgr.make_windowed(movie_paths, scale=scale, vec_class=SubprocVecEnv)
         assert hasattr(env, 'window')
-        
+
         obs = env.reset()
         assert env.window is not None
-        env.window.show_menu = True
+        env.window.show_menu = not reuse_save_slots
         callback = WindowUpdateCallback(env.window)
     else:
         env = cast(GymEnv, mgr.make(movie_paths, vec_class=SubprocVecEnv))
         obs = env.reset()
+        callback = WindowUpdateCallback(None)
 
     assert env is not None
-    model = algo_class("MultiInputPolicy", env=env, verbose=int(verbose), **algo_kwargs)
-    
+    model = algo_class("MultiInputPolicy", env=env, verbose=int(verbose), device=device, **algo_kwargs)
+    if load_model_path is not None and load_model_path.exists():
+        try:
+            model.load(load_model_path)
+        except Exception as e:
+            print(f"Failed to load model from {load_model_path}: {e}")
+
     while True:
         if isinstance(env, (SubprocVecEnv, VecWindowWrapperSB3)):
             dummy_actions = np.array([0 for _ in range(env.num_envs)])
             obs, rewards, dones, infos = env.step(dummy_actions)
-        
+
         if window and hasattr(env, 'window') and env.window is not None:
-            assert isinstance(env, (WindowWrapper, VecWindowWrapperSB3))
+            assert isinstance(env, VecWindowWrapperSB3)
             env.window.update()
-            
+
         all_ready = np.all(dones)
         if all_ready:
             env.env_method("enable")
@@ -83,27 +115,28 @@ def train_rl(
     try:
         print("Starting RL training. Press Ctrl+C to exit.")
         # total_timesteps should be passed via args
-        model.learn(total_timesteps=epochs, callback=callback)
+        model.learn(total_timesteps=total_timesteps, callback=callback)
+        if save_model_path is not None:
+            model.save(save_model_path)
     except KeyboardInterrupt:
         print("Training interrupted by user.")
     finally:
-        if isinstance(env, (WindowWrapper, VecWindowWrapper)):
-            assert env.window is not None
-            env.window.destroy()
+        env.close()
 
 
 train_rl_parser = ArgumentParser(add_help=False)
-train_rl_parser.add_argument("--sample-from", type=Path, help="movie file that performs menuing and single race lap to collect savestates for sampling position, if none is specified, keyboard input will be enable for the menu and first lap before training.")
-train_rl_parser.add_argument("--algorithm", choices=ALGO_MAP.keys(), help="training algorithm", default="ppo")
-train_rl_parser.add_argument("--epochs", type=int, help="number of training epochs", default=EPOCHS)
+train_rl_parser.add_argument("--sample-from", type=Path, help="movie file that performs menuing and single race lap to collect savestates for sampling position, if none is specified, keyboard input will be enable for the menu and first lap before training.", default=SUPPRESS)
+train_rl_parser.add_argument("--algorithm", choices=ALGO_MAP.keys(), help="training algorithm", default=SUPPRESS)
+train_rl_parser.add_argument("--total-timesteps", type=int, help="number of training timesteps", default=SUPPRESS)
 train_rl_parser.add_argument("--window", "-w", action="store_true", help="display a window showing the agent's environment")
 train_rl_parser.add_argument("--reuse-save-slots", action="store_true", help="reuse save slots for training, if not specified, new save slots will be created for each training session. If running headless, this will be forcibly enabled.")
 train_rl_parser.add_argument(
     "--num-procs",
     help="Specify the number of processes to debug an emulator on. NOTE: play mode does not support multiple processes",
-    type=int,
-    default=1
+    type=int
 )
+train_rl_parser.add_argument("--save-model-path", type=Path, help="Path to save the trained model")
+train_rl_parser.add_argument("--load-model-path", type=Path, help="Path to load a pre-trained model")
 train_rl_parser.set_defaults(func=train_rl, env_name="gym_mkds/MarioKartDS-v1")
 
 
